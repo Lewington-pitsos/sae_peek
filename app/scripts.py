52f57efe698473876ef1ef3da3fe4253e375b258
Louka Ewington-Pitsos <lewingtonpitsos@gmail.com>
2024-07-17 12:13:26 +1000

    add script to download aseop

--- app/scripts.py
+++ app/scripts.py
@@ -1,5 +1,62 @@
 from datasets import load_dataset
-from torch.utils.data import DataLoader
+import os
+import requests
+import json
+import time
+from torch.utils.data import DataLoader, Dataset
+
+AESOP_URL = "https://raw.githubusercontent.com/itayniv/aesop-fables-stories/master/public/aesopFables.json"
+LOCAL_AESOP_FILENAME = os.path.join('data', "aesopFables.json")
+
+class _FablesDataset(Dataset):
+    def __init__(self, fables):
+        self.fables = fables['stories']
+
+    def __len__(self):
+        return len(self.fables)
+
+    def __getitem__(self, idx):
+        fable = self.fables[idx]
+        return fable['title'] + "\n" + " ".join(fable['story'])
+
+def _download_aesop(url, local_filename, max_retries=5):
+    retries = 0
+    while retries < max_retries:
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            with open(local_filename, 'wb') as file:
+                file.write(response.content)
+            return
+        except requests.ConnectionError:
+            retries += 1
+            print(f"Connection error. Retrying {retries}/{max_retries}...")
+            time.sleep((2 ** retries)  / 4)
+    raise Exception(f"Failed to download file after {max_retries} attempts.")
+
+
+def _aesop_dataloader(fables, tokenizer, batch_size, sequence_length):
+    dataset = _FablesDataset(fables)
+
+    def tokenize(batch):
+        return tokenizer(batch, padding='longest', truncation=True, max_length=sequence_length, return_tensors='pt')
+
+    dl = DataLoader(dataset, batch_size=batch_size, shuffle=False, collate_fn=tokenize)
+    return dl
+
+def load_aesop(tokenizer, batch_size, sequence_length):
+    if not os.path.exists(LOCAL_AESOP_FILENAME):
+        print(f"{LOCAL_AESOP_FILENAME} does not exist. Downloading...")
+        _download_aesop(AESOP_URL, LOCAL_AESOP_FILENAME)
+
+    with open(LOCAL_AESOP_FILENAME, 'r') as file:
+        data = json.load(file)
+
+    dataloader = _aesop_dataloader(data, tokenizer, batch_size, sequence_length)
+    return dataloader
+
+
+
 
 def load_pile10k(tokenizer, batch_size, sequence_length, num_samples=5000):
     dataset = load_dataset('NeelNanda/pile-10k', split="train")
--- app/test_scripts.py
+++ app/test_scripts.py
@@ -1,12 +1,17 @@
 from transformers import GPT2Tokenizer
-from app.scripts import *
+from app.scripts import load_aesop, load_pile10k
 import pytest
 
-def test_loads_pile10k():
+# tokenizer fixture
+@pytest.fixture(scope='module')
+def tokenizer():
+    tok = GPT2Tokenizer.from_pretrained('gpt2')
+    tok.pad_token = tok.eos_token
+    return tok
+
+def test_loads_pile10k(tokenizer):
     pytest.skip("Test takes too long")
     batch_size = 32
-    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-    tokenizer.pad_token = tokenizer.eos_token
 
     dl = load_pile10k(tokenizer, 256, batch_size, num_samples=128)
 
@@ -16,6 +21,18 @@ def test_loads_pile10k():
 
     assert n['input_ids'].shape[0] == batch_size
     assert n['attention_mask'].shape[0] == batch_size
-
-    # assert no other keys in n
     assert len(n.keys()) == 2
+
+def test_loads_aesop(tokenizer):
+    batch_size = 32
+    sequence_length = 1024
+    dl = load_aesop(tokenizer, batch_size, sequence_length)
+
+    n = next(iter(dl))
+
+    print(n)
+
+    assert n['input_ids'].shape[0] == batch_size
+    assert n['attention_mask'].shape[0] == batch_size
+    assert len(n.keys()) == 2
+
